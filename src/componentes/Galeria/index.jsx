b6a8962064fd4d151c1d180667dfb3a8f5520039
Juliana Lucca <lucca.editora@gmail.com>
2023-04-27 15:17:15 -0300

    create Cards component

--- src/componentes/Galeria/index.jsx
+++ src/componentes/Galeria/index.jsx
@@ -2,34 +2,15 @@ import React from 'react'
 import styles from './Galeria.module.scss';
 import Tags from '../Tags';
 import fotos from './fotos.json';
-import open from './open.png';
-import favorito from './favorito.png';
+
+import Cards from './Cards';
 
 export default function Galeria() {
   return (
     <section className={styles.galeria}>
         <h2>Navegue pela galeria</h2>
         <Tags />
-        <ul className={styles.galeria__cards}>
-          {fotos.map((foto) => {
-            return (
-              <li key={foto.id} className={styles.galeria__card}>
-                <img 
-                  src={foto.imagem}
-                  alt={foto.titulo}
-                  className={styles.galeria__imagem} />
-                <p className={styles.galeria__descricao}>{foto.titulo}</p>
-                <div>
-                  <p>{foto.creditos}</p>
-                  <span>
-                    <img src={favorito} alt="Ícone coração de curtir" />
-                    <img src={open} alt="ícone de abrir modal" />
-                  </span>
-                </div>
-              </li>
-            )
-          })}
-        </ul>
+        <Cards itens={fotos} styles={styles}/>
     </section>
   )
 }
